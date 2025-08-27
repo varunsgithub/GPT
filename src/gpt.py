1cd39e0647438fb924f3a4a41b094b0d4c453c17
varunsingh <varsingh@seas.upenn.edu>
2025-08-27 17:53:47 -0400

    Updated the feed forward mechanism for the model

--- src/gpt.py
+++ src/gpt.py
@@ -78,3 +78,24 @@ class LayerNorm(nn.Module):
         var = x.var(dim=-1, keepdim=True, unbiased = False)
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.scale * norm_x + self.shift
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+#Class for feedforward mech
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+                                    GELU(),
+                                    nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]))
+        
+    def forward(self, x):
+        return self.layers(x)
