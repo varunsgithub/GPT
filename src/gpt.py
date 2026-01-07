456b80fb3ebb40abe0cce751e0e28b7dd023dbfd
varunsingh <varsingh@seas.upenn.edu>
2026-01-07 23:38:53 +0530

    Training the model

--- src/gpt.py
+++ src/gpt.py
@@ -1,76 +1,53 @@
-"""Tokenizer Class"""
-
-import re
-import tiktoken
 import torch
-from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 
-class SimpleTokenizerV1:
-    def __init__(self,vocab):
-        self.str_to_int = vocab
-        ##This is just a reverse dictionary
-        self.int_to_str = { i : s for s, i in vocab.items()}
-    
-    def encode(self, text):
-        """Splitting the text into tokens and encoding them"""
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        # The scale is the variance module (learned weights)
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        # The shift is the mean module (learned weights)
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
 
-        ##Adding the <UNK> tags check in vocab.
-        ## The tags have to be present in the vocab, this is just a placeholder.
-        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        var = x.var(dim = -1, keepdim = True, unbiased = False)
+        # Take each value, subtract it by the mean and divide it by the standard deviation
+        norm_x = (x-mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
 
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-    
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids]) 
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
-        return text
-    
 
-    """Dataset and Dataloader classes"""
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
 
-class GPTDatasetV1(Dataset):
-    def __init__(self, txt, tokenizer, max_length, stride):
-        self.input_ids = []
-        self.target_ids = []
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
 
-        token_ids = tokenizer.encode(txt)    
-
-        for i in range(0, len(token_ids) - max_length, stride):     
-            input_chunk = token_ids[i:i + max_length]
-            target_chunk = token_ids[i + 1: i + max_length + 1]
-            self.input_ids.append(torch.tensor(input_chunk))
-            self.target_ids.append(torch.tensor(target_chunk))
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, idx):         
-        return self.input_ids[idx], self.target_ids[idx]
-    
-    def create_dataloader_v1(txt, batch_size=4, max_length=256,
-                            stride=128, shuffle=True, drop_last=True,
-                            num_workers=0):
-        tokenizer = tiktoken.get_encoding("gpt2")                         
-        dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)   
-        dataloader = DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=shuffle,
-            drop_last=drop_last,     
-            num_workers=num_workers     
+class FeedForward(nn.Module):
+    def __init__(self,cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            # The linear layer transforms the attention tokens into 4 * emb_dim dimensions
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            # The layer normalizes it
+            GELU(),
+            # The linear layer then transforms the 4 * emb dimensions into the emb_dim
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
         )
-        return dataloader
-    
 
-"""Self attention class (multiheaded)"""
+    def forward(self, x):
+        return self.layers(x)
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, 
                  context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
+        ##So that we have balanced split for the heads.
         assert (d_out % num_heads == 0), \
             "d_out must be divisible by num_heads"
 
@@ -96,9 +73,9 @@ class MultiHeadAttention(nn.Module):
 
         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
         values = values.view(b, num_tokens, self.num_heads, self.head_dim)  
-        queries = queries.view(                                             
+        queries = queries.view(                                          
             b, num_tokens, self.num_heads, self.head_dim                    
-        )                                                                   
+        )                                             
 
         keys = keys.transpose(1, 2)
         queries = queries.transpose(1, 2)
@@ -114,9 +91,66 @@ class MultiHeadAttention(nn.Module):
         attn_weights = self.dropout(attn_weights)
 
         context_vec = (attn_weights @ values).transpose(1, 2)
- #10
+        
         context_vec = context_vec.contiguous().view(
             b, num_tokens, self.d_out
         )
         context_vec = self.out_proj(context_vec)
         return context_vec
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x     
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+        torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
